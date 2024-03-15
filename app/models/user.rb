9850917817509b8e3a07dd9da1536fa4951fa1a6
Yokko405 <hiyume.2@gmail.com>
2024-03-15 16:00:19 +0900

    ユーザー新規登録

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :kana_last_name, presence: true
+  validates :kana_first_name, presence: true
+  validates :birthday, presence: true
+
+  # validates :password, length: { minimum: 6, maximum: 128 }, if: -> { password.present? }
+
+  has_many :items
+
 end
