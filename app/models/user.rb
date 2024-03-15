1fa8db05ed5342eb7bebc9e3d3b3ae0cff7dc3fa
Yokko405 <hiyume.2@gmail.com>
2024-03-15 21:18:42 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :kana_last_name, presence: true
-  validates :kana_first_name, presence: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー－]+\z/, message: 'は全角で入力してください' }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー－]+\z/, message: 'は全角で入力してください' }
+  validates :kana_last_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力してください' }
+  validates :kana_first_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力してください' }
   validates :birthday, presence: true
 
-  # validates :password, length: { minimum: 6, maximum: 128 }, if: -> { password.present? }
+  # 半角英数字混合のバリデーション
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数字混合で入力してください。' }
 
   has_many :items
 
