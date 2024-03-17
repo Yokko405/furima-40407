e2d2dfbcc013b27e5a8917a69e4be199a2f4518c
Yokko405 <hiyume.2@gmail.com>
2024-03-17 19:26:51 +0900

    テストコード追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true, format: { with: /\A[^\x20-\x7E｡-ﾟ]+\z/, message: 'は全角文字で入力してください' }
-  validates :first_name, presence: true, format: { with: /\A[^\x20-\x7E｡-ﾟ]+\z/, message: 'は全角文字で入力してください' }
-  validates :kana_last_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力してください' }
-  validates :kana_first_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力してください' }
+  validates :last_name, presence: true, format: { with: /\A[^\x20-\x7E｡-ﾟ]+\z/, message: 'は全角文字で入力してください。' }
+  validates :first_name, presence: true, format: { with: /\A[^\x20-\x7E｡-ﾟ]+\z/, message: 'は全角文字で入力してください。' }
+  validates :kana_last_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力してください。' }
+  validates :kana_first_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力してください。' }
   validates :birthday, presence: true
 
   # 半角英数字混合のバリデーション
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,10 +27,59 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include 'Email has already been taken'
       end
+      it 'emailに@を含まない場合は登録できない' do
+        @user.email = 'EMAIL.com'
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Email is invalid"
+      end
       it 'passwordが空では登録できない' do
         @user.password = '' # passwordの値を空にする
         @user.valid?
         expect(@user.errors.full_messages).to include "Password can't be blank"
+      end
+      it 'passwordが6文字未満では登録できない' do
+        @user.password = 'pas12'
+        @user.password_confirmation = 'pas12'
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Password is too short (minimum is 6 characters)"
+      end
+      it '英字のみのパスワードでは登録できない' do
+        @user.password = 'Applered'
+        @user.password_confirmation = 'Applered'
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Password は半角英数字混合で入力してください。"
+      end
+      it '数字のみのパスワードでは登録できない' do
+        @user.password = '12345678'
+        @user.password_confirmation = '12345678'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password は半角英数字混合で入力してください。')
+      end
+      it '全角文字を含むパスワードでは登録できない' do
+        @user.password = 'passワード1234'
+        @user.password_confirmation = 'passワード1234'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password は半角英数字混合で入力してください。')
+      end
+      it '姓（全角）に半角文字が含まれていると登録できない' do
+        @user.last_name = 'yamada'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Last name は全角文字で入力してください。')
+      end
+      it '名（全角）に半角文字が含まれていると登録できない' do
+        @user.first_name = 'taro'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('First name は全角文字で入力してください。')
+      end
+      it '姓（カナ）にカタカナ以外の文字（平仮名・漢字・英数字・記号）が含まれていると登録できない' do
+        @user.kana_last_name = 'やまだ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Kana last name は全角カタカナで入力してください。')
+      end
+      it '名（カナ）にカタカナ以外の文字（平仮名・漢字・英数字・記号）が含まれていると登録できない' do
+        @user.kana_first_name = 'たろう '
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Kana first name は全角カタカナで入力してください。')
       end
       it 'passwordとpassword_confirmationが一致しない場合は登録できない' do
         @user.password = 'PASSWORD1' # passwordを設定する
