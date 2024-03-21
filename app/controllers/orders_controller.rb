af19324881874770d11b1e28ca933cb531fd6c48
Yokko405 <hiyume.2@gmail.com>
2024-03-21 15:35:57 +0900

    フォームオブジェクト

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,25 +4,30 @@ class OrdersController < ApplicationController
   before_action :redirect_if_seller
 
   def index
-    @shipping_address = ShippingAddress.new
     @order_form = OrderForm.new
   end
   
 
   def create
-    binding.pry
-    @shipping_address = ShippingAddress.new(shipping_address_params)
-    if @shipping_address.valid?
-      @shipping_address.save
-      return redirect_to root_path
+    @order_form = OrderForm.new(order_form_params)
+    if @order_form.valid?
+      Payjp.api_key = "sk_test_0f996e2c15d176b269d7029a"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+      Payjp::Charge.create(
+        amount: order_form_params[:price],  # 商品の値段
+        card: order_form_params[:token],    # カードトークン
+        currency: 'jpy'                 # 通貨の種類（日本円）
+      )
+      @order_form.save
+      redirect_to root_path
     else
       render 'index', status: :unprocessable_entity
     end
   end
 
   private
-  def shipping_address_params
-    params.require(:shipping_address).permit(:purchase_record, :postal_code, :prefecture_id, :city, :address_number, :building_name, :phone_number, :item_price).merge(token: params[:token])
+  def order_form_params
+    params.require(:order_form).permit(:user_id, :item_id, :postal_code, :prefecture_id, :city, :address_number, :building_name, :phone_number)
+      .merge(item_id: @item.id, user_id: current_user.id, token: params[:token])
   end
 
   def set_item
--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,16 +1,14 @@
 class ShippingAddress < ApplicationRecord
   attr_accessor :token
-  validates :price, presence: true
 
   belongs_to :purchase_record
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
-  validates :prefecture_id, presence: true
 
-  validates :postal_code, presence: true
+  validates :postal_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/ }
+  validates :prefecture_id, presence: true
   validates :city, presence: true
   validates :address_number, presence: true
-  validates :phone_number, presence: true
-
+  validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/ }
 end
